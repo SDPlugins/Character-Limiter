e6272c3c116bcf8a12d71daad0e1bdd73d938135
SDPlugins <admin@sdplugins.com>
2017-06-06 10:12:31 -0400

    Init Commit

---
+++ Handler.cs
@@ -0,0 +1,54 @@
+﻿using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SDPlugins
+{
+
+    public class Handler
+    {
+        public static void PlayerJoined(UnturnedPlayer player)
+        {
+            SteamPlayer splayer = PlayerTool.getSteamPlayer(player.CSteamID);
+            SerializableDictionary<CSteamID, List<bool>> totalCharacters = Init.instance.Configuration.Instance.totalCharacters;
+
+            if (totalCharacters.ContainsKey(player.CSteamID))
+            {
+                int count = 0;
+                for (int i = 0; i < totalCharacters[player.CSteamID].Count; i++)
+                {
+                    if (totalCharacters[player.CSteamID][i] && i != splayer.playerID.characterID)
+                        count++;
+                }
+
+                if (count > Init.instance.Configuration.Instance.CharAmount - 1)
+                {
+                    Init.instance.Kick(player);
+                }
+                else
+                {
+                    totalCharacters[player.CSteamID][splayer.playerID.characterID] = true;
+                }
+            }
+            else
+            {
+                totalCharacters.Add(player.CSteamID, new List<bool>()
+                {
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                });
+
+                totalCharacters[player.CSteamID][splayer.playerID.characterID] = true;
+            }
+            Init.instance.Configuration.Instance.totalCharacters = totalCharacters;
+            Init.instance.Configuration.Save();
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using Rocket.Core;
+using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using UnityEngine;
+using System.Xml.Serialization;
+using Steamworks;
+using Rocket.API.Collections;
+using Rocket.Unturned;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SDPlugins
+{
+    public class CharLimiterConfig : IRocketPluginConfiguration
+    {
+        public int CharAmount;
+        public SerializableDictionary<CSteamID, List<bool>> totalCharacters;
+
+        public void LoadDefaults()
+        {
+            CharAmount = 2;
+            totalCharacters = new SerializableDictionary<CSteamID, List<bool>>();
+        }
+    }
+    public class Init : RocketPlugin<CharLimiterConfig>
+    {
+        public static Init instance;
+        protected override void Load()
+        {
+            instance = this;
+            U.Events.OnPlayerConnected += Handler.PlayerJoined;
+        }
+        protected override void Unload()
+        {
+            U.Events.OnPlayerConnected -= Handler.PlayerJoined;
+        }
+
+        public override TranslationList DefaultTranslations 
+        {
+            get
+            {
+                return new TranslationList()
+                {
+                    {"char_limit_exceeded_kick", "You are only allowed to have {0} characters in the server." }
+                };
+            }
+        }
+        public void Kick(UnturnedPlayer player)
+        {
+            StartCoroutine(KickPlayer(player));
+        }
+        public IEnumerator KickPlayer(UnturnedPlayer player)
+        {
+            yield return new WaitForSeconds(10);
+            Provider.kick(player.CSteamID, Translate("char_limit_exceeded_kick", Configuration.Instance.CharAmount));
+        }
+
+    }
+    [XmlRoot("Dictionary")]
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IXmlSerializable
+    {
+        #region IXmlSerializable Members
+        public System.Xml.Schema.XmlSchema GetSchema()
+        {
+            return null;
+        }
+        public void ReadXml(System.Xml.XmlReader reader)
+        {
+            XmlSerializer keySerializer = new XmlSerializer(typeof(TKey));
+            XmlSerializer valueSerializer = new XmlSerializer(typeof(TValue));
+            bool wasEmpty = reader.IsEmptyElement;
+            reader.Read();
+            if (wasEmpty)
+                return;
+            while (reader.NodeType != System.Xml.XmlNodeType.EndElement)
+            {
+                reader.ReadStartElement("item");
+                reader.ReadStartElement("key");
+                TKey key = (TKey)keySerializer.Deserialize(reader);
+                reader.ReadEndElement();
+                reader.ReadStartElement("value");
+                TValue value = (TValue)valueSerializer.Deserialize(reader);
+                reader.ReadEndElement();
+                this.Add(key, value);
+                reader.ReadEndElement();
+                reader.MoveToContent();
+            }
+            reader.ReadEndElement();
+        }
+        public void WriteXml(System.Xml.XmlWriter writer)
+        {
+            XmlSerializer keySerializer = new XmlSerializer(typeof(TKey));
+            XmlSerializer valueSerializer = new XmlSerializer(typeof(TValue));
+            foreach (TKey key in this.Keys)
+            {
+                writer.WriteStartElement("item");
+                writer.WriteStartElement("key");
+                keySerializer.Serialize(writer, key);
+                writer.WriteEndElement();
+                writer.WriteStartElement("value");
+                TValue value = this[key];
+                valueSerializer.Serialize(writer, value);
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+            }
+        }
+        #endregion
+    }
+}
